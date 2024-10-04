a56b5d8c32700e39102650af5dbd18f4bed5a362
Isaac Benavides <isaac_al_saca@hotmail.com>
2024-10-04 16:34:52 -0600

    App done

---
+++ app.js
@@ -0,0 +1,102 @@
+const input_field = document.querySelector("input")
+const add_button = document.querySelector("#agregar")
+const lista = document.querySelector("#lista")
+
+const crear_tarea = (tarea, done = false) => {
+    const li = document.createElement("li")
+    const p = document.createElement("p")
+    const button = document.createElement("button")
+    p.textContent = tarea
+
+    if (done) {
+        p.style.textDecoration = "line-through"
+    }
+    
+    button.textContent = '-'
+    button.addEventListener("click", delete_item)
+
+    p.addEventListener("click", toggle_tarea)
+    li.appendChild(p)
+    li.appendChild(button)
+
+    lista.appendChild(li)
+}
+
+add_button.addEventListener("click", (event) => {
+    event.preventDefault()
+
+    const errorMsg = document.querySelector("#error-msg")
+
+    if (input_field.value === '') {
+        errorMsg.textContent = "No se puede agregar una tarea vacia"
+        return 
+    }
+    
+    errorMsg.textContent = ""
+
+    crear_tarea(input_field.value)
+
+    input_field.value = ""
+
+    verificar_lista_vacia()
+    hacer_backup_local_storage()
+})
+
+const hacer_backup_local_storage = () => {
+    const tareas = document.querySelectorAll("li p")
+    const backup = []
+
+    for (let i = 0; i < tareas.length; i++) {
+        let aux = {
+            tarea: tareas[i].childNodes[0].nodeValue,
+            done: false
+        }
+        
+        if (tareas[i].style.textDecoration) {
+            aux.done = true
+        }
+
+        backup.push(aux)
+    }
+
+    localStorage.setItem("tareas", JSON.stringify(backup))
+}
+
+const obtener_backup_local_storage = () => {
+    const tareas = localStorage.getItem("tareas")
+
+    const tareasObj = JSON.parse(tareas)
+
+    if (tareasObj) {
+        tareasObj.forEach((tarea) => crear_tarea(tarea?.tarea, tarea.done))
+    }
+
+    verificar_lista_vacia()
+}
+
+const verificar_lista_vacia = () => {
+    const tareas = document.querySelectorAll("li")
+    const mensaje = document.querySelector("#lista-vacia")
+
+    if (tareas.length === 0) {
+        mensaje.style.display = "block";
+    } else {
+        mensaje.style.display = "none";
+    }
+}
+
+const toggle_tarea = (event) => {
+    event.target.style.textDecoration = "line-through"
+
+    hacer_backup_local_storage()
+}
+
+const delete_item = (event) => {
+    const tarea = event.target.parentElement
+    lista.removeChild(tarea)
+
+    verificar_lista_vacia()
+    hacer_backup_local_storage()
+}
+
+obtener_backup_local_storage()
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Lista de Tareas
+## Trabajo 03 - Programación para Internet
+
+### Alumno
+Isaac Alain Benavides Hernandez
+216817953
+
+Enlace a la página: 
+
+#### Instrucciones
+La lista de tareas esta desarrollada con JavaScript Vanilla, por lo que para ejecutar la app solo se tiene que abrir el archivo index.html en el navegador, o se puede abrir el enlace de arriba para probar el funcionamiento.
+
+#### Funcionamiento
+1. Se pueden agregar tareas nuevas escribiendo en el campo de texto y dando click en el botón "+".
+
+![Agregando una tarea a la lista](images/01.png)
+
+Para marcar tareas como completadas se puede dar click en el texto de la tarea que ha sido agregada. Las tareas completadas se mostraran con el texto tachado.
+
+![Completando 2 tareas de la lista](images/02.png)
+
+Si se intenta agregar una tarea y no se ha ingresado nada en el campo te texto, se mostrará una advertencia arriba del campo de texto.
+
+![Advertencia sobre tarea vacia](images/03.png)
+
+Si se quiere eliminar una tarea, se puede utilizar el botón "-" que esta al final de cada tarea.
+
+![Eliminación de una tarea](images/04.png)
+
+Esta lista de tareas funciona con localStorage, por lo que si se cierra el navegador o se recarga la página no se borran las tareas guardadas previamente. Además de que se añadieron algunos estilos para que se adaptara a resoluciones más pequeñas que las de una computadora.
+
+![Responsividad puesta a prueba](images/05.png)
